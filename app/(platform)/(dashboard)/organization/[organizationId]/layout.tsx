4f5d9b997e362342526fc2545f9769e85396ef8a
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-02-03 20:49:04 +0100

    Add the sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,16 @@
+import OrgControl from "./_components/OrgControl";
+
+const OrganizationIdLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
+export default OrganizationIdLayout;
