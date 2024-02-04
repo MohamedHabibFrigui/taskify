954ae3cece0a9142191e9b227ad0887cc099247f
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-02-04 09:55:52 +0100

    Add sidebar skeleton when loading

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -46,7 +46,15 @@ const Sidebar = ({ storageKey = "taskify-sidebar-state" }: SidebarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-1/2" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
