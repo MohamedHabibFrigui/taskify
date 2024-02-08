fb9ade1a29c067a1f8202535f8e97622d48bb0a5
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-02-08 19:52:53 +0100

    Add board navbar

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardTitleForm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import FormInput from "@/components/form/FormInput";
+import { Button } from "@/components/ui/button";
+import { Board } from "@prisma/client";
+import { ElementRef, useRef, useState } from "react";
+
+interface BoardTitleFormProps {
+  data: Board;
+}
+
+const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    console.log("I am submitted", title);
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
+  if (isEditing) {
+    return (
+      <form
+        action={onSubmit}
+        ref={formRef}
+        className="flex items-center gap-x-2"
+      >
+        <FormInput
+          ref={inputRef}
+          id="title"
+          onBlur={onBlur}
+          defaultValue={data.title}
+          className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
+        />
+      </form>
+    );
+  }
+
+  return (
+    <Button
+      onClick={enableEditing}
+      className="font-bold text-lg h-auto w-auto p-1 px-2"
+      variant="transparent"
+    >
+      {data.title}
+    </Button>
+  );
+};
+
+export default BoardTitleForm;
