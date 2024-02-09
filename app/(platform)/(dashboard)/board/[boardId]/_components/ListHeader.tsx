3028a862785852cd343c6ad9ee95b3c807489934
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-02-09 19:02:38 +0100

    Add list options ui (add, copy or delete a list)

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
@@ -7,6 +7,7 @@ import { List } from "@prisma/client";
 import { ElementRef, useRef, useState } from "react";
 import { toast } from "sonner";
 import { useEventListener } from "usehooks-ts";
+import ListOptions from "./ListOptions";
 
 interface ListHeaderProps {
   data: List;
@@ -90,6 +91,7 @@ const ListHeader = ({ data }: ListHeaderProps) => {
           {title}
         </div>
       )}
+      <ListOptions onAddCard={() => {}} data={data} />
     </div>
   );
 };
