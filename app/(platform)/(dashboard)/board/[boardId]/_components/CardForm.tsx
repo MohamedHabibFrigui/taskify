c272f29598085f3279e7d341e96d7c20f58fb87f
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-02-09 20:31:13 +0100

    Add create card action and display the cards

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { createCard } from "@/actions/create-card";
 import FormSubmit from "@/components/form/FormSubmit";
 import FormTextarea from "@/components/form/FormTextarea";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
 import { Plus, X } from "lucide-react";
-import { forwardRef } from "react";
+import { useParams } from "next/navigation";
+import { ElementRef, KeyboardEventHandler, forwardRef, useRef } from "react";
+import { toast } from "sonner";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
 
 interface CardFormProps {
   listId: string;
@@ -15,14 +20,58 @@ interface CardFormProps {
 
 const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+    const params = useParams();
+    const formRef = useRef<ElementRef<"form">>(null);
+
+    const { execute, fieldErrors } = useAction(createCard, {
+      onSuccess: (data) => {
+        toast.success(`Card "${data.title}" created`);
+        formRef.current?.reset();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    });
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        disableEditing();
+      }
+    };
+
+    useOnClickOutside(formRef, disableEditing);
+    useEventListener("keydown", onKeyDown);
+
+    const onTextAreaKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (
+      e
+    ) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        formRef.current?.requestSubmit();
+      }
+    };
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const listId = formData.get("listId") as string;
+      const boardId = params.boardId as string;
+
+      execute({ title, listId, boardId });
+    };
+
     if (isEditing) {
       return (
-        <form className="m-1 py-0.5 px-1 space-y-4">
+        <form
+          ref={formRef}
+          action={onSubmit}
+          className="m-1 py-0.5 px-1 space-y-4"
+        >
           <FormTextarea
             id="title"
-            onKeyDown={() => {}}
+            onKeyDown={onTextAreaKeyDown}
             ref={ref}
             placeholder="Enter a title for this card..."
+            errors={fieldErrors}
           />
           <input hidden id="listId" name="listId" value={listId} />
           <div className="flex items-center gap-x-1">
@@ -34,6 +83,7 @@ const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
         </form>
       );
     }
+
     return (
       <div className="pt-2 px-2">
         <Button
